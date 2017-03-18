d91fbedda45fc9ad2417c9282da491f4b4b24eb1
Lindsey Handley <lindseyhandley@gmail.com>
2017-03-18 14:36:24 -0700

    first draft of Gambit Queen landing page

--- containers/app.tsx
+++ containers/app.tsx
@@ -4,17 +4,44 @@ class App extends React.Component<any,any> {
   render() {
 
     return (
-      <div style={{width:"100%"}}>
+      <div>
+        <div style={{position:'absolute'}}>
+          <h1 style={{
+            textAlign:'center',
+            paddingLeft:"10%",
+            paddingRight:"50%",
+            paddingTop:"3em"
+          }}>
+            Gambit Queen is Now Available on Kindle!
+          </h1>
+          <h3 style={{
+            textAlign:'center',
+            paddingLeft:"10%",
+            paddingRight:"50%"
+          }}>A young drug dealer with genetic gifts that she herself doesn't fully understand is coerced into attending a top-secret school for incubating covert operatives. She finds herself part of a deadly geo-political game of chess played by the school's headmaster. Before the world is consumed by a Third World War, she must transcend the game, becoming a chess player in her own right, not just a queen upon a global chessboard.</h3>
+          <p style={{
+            textAlign:'center',
+            paddingLeft:"10%",
+            paddingRight:"50%" 
+          }}>"I loved every minute of it!" -Mrs. S</p>
+          <button style={{
+            marginLeft:"23%",
+            marginRight:"50%",
+            outline: 0,
+            background:'none', 
+            border:'2px solid #F7CA18',
+            padding: '10px 35px',
+            fontSize:15,
+            color:'#F7CA18',
+            cursor:'pointer',
+            borderRadius:50,
+            transition: 'all 0.3s ease'
+          }}>Buy for Kindle</button>
+        </div>
         <img src="/assets/GambitQueenLandingPage.png" style={{
-          position:'absolute'
+          width:"100%",
+          margin: "auto"
         }} />
-        <h1 style={{
-          position:'absolute',
-          marginTop:100,
-          marginLeft:50
-        }}>
-          Buy Gambit Queen Now on Kindle!
-        </h1>
       </div>
     );
   };
